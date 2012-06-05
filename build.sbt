7eb35602231372f56d50bedc2454c106c2e689a5
Johannes Rudolph <johannes_rudolph@gmx.de>
2012-06-05 13:53:45 +0200

    minimal sbt project
     * build.sbt
     * idea artifacts ignored
     * wrap testng tests in scalatest tests
     * all tests still failing due to some NPE problem

---
+++ build.sbt
@@ -0,0 +1,6 @@
+libraryDependencies ++= Seq(
+  "org.parboiled" % "parboiled-java" % "1.0.2",
+  "org.testng" % "testng" % "5.14.10" % "test",
+  "net.sf.jtidy" % "jtidy" % "r938" % "test",
+  "org.scalatest" % "scalatest_2.9.1" % "1.8" % "test"
+)
---
+++ src/test/scala/ScalaTestNgSuite.scala
@@ -0,0 +1,11 @@
+package org.pegdown
+
+import org.scalatest.testng.TestNGSuite
+
+class ScalaCustomParserTest extends CustomParserTest with TestNGSuite
+class ScalaMarkdown103Test extends Markdown103Test with TestNGSuite
+class ScalaMarukuTest extends MarukuTest with TestNGSuite
+class ScalaPathologicalHtmlTest extends PathologicalHtmlTest with TestNGSuite
+class ScalaPegDownTest extends PegDownTest with TestNGSuite
+class ScalaPhpMarkdownExtraTest extends PhpMarkdownExtraTest with TestNGSuite
+class ScalaPhpMarkdownTest extends PhpMarkdownTest with TestNGSuite
